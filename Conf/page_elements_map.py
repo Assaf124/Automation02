a8b47739e7265f840f5f77bf5141873f14382de4
ASSAF ALONI <39213477+Assaf124@users.noreply.github.com>
2021-06-01 22:36:41 +0300

    Update page_elements_map.py

--- Conf/page_elements_map.py
+++ Conf/page_elements_map.py
@@ -12,11 +12,19 @@ page_elements = {'main_page_verifier_alternative_1': '//*[@id="app"]/div/div[3]/
                  'start_stop_scan_button_old': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div/div/div[1]/div[2]/button',
                  'clear_search_button': '//*[@id="app"]/div/div[3]/div[1]/div[2]/div/div/div[2]/div[2]/div/div/div[1]/div[2]/div[2]/a',
                  'network_list_view': '//*[@id="app"]/div/div[3]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div[1]/div[1]/div/ul/li[1]/div[3]/h3/div[2]',
+                 'network_search_box': '//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/ul/li[2]/input',
                  'session_name':'//*[@id="app"]/div/div[3]/div[1]/div[2]/div/div/div[1]/div[1]/div/span',
-                 'table_old': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[1]/span[4]/span[2]',
-                 'table': '//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/span[2]/i',
+                 'table_old': '//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/span[2]',
+                 'table': '//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/span[2]/i',
                  'grid': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[1]/span[4]/span[1]',
-                 'edit_session_pencil_old': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div[2]/div/div[1]/div[1]/a',
+                 'search': '/html/body/div[2]/div/div/div[3]/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div/div/div[1]/div/div[1]/div[2]/span/div/div[1]/div[1]/div[1]/input',
                  'edit_session_pencil': '//*[@id="app"]/div/div/div[3]/div[1]/div[1]/div[2]/div/div[1]/div[1]/a',
                  'device_list_top': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[1]/div/table/tbody/tr/td[3]/div',
                  'empty_device_list': '//*[@id="app"]/div/div[3]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[1]/div/table/tbody/tr[1]/td/div'}
+
+
+load_page_element = {
+                    'login_page': 'button',
+                    'cube_page': 'img',
+                    'sensors_page': 'button'
+                    }
